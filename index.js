9b077f275e0ea390aadea7c8b4a2380a6458a241
Zakaria Karlsen Tawfiq <zaktaw@gmail.com>
2021-03-15 18:32:42 +0100

    Bot is now using environment variables instead of a config file

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const Discord = require('discord.js');
-const config = require('./config.json')
 const wyr = require('./wyr/wyr.js')
 const nhie = require('./nhie/nhie.js')
 const bot = new Discord.Client();
-
-TOKEN = config.token;
-PREFIX = config.prefix;
-CHANNEL_ID = config.channelID
 
 bot.on('ready', () => {
     console.log("Bot is online!");
 }); 
 
-bot.login(TOKEN);
+
+bot.login(process.env.DRINKING_GAME_BOT_TOKEN);
 
 bot.on('message', (msg) => {
 
-    let args = msg.content.substring(PREFIX.length).split(" ");
+    let args = msg.content.split(" ");
 
     // Prevent spam from bot
     if (msg.author.bot) return; // stops bot from replying to itself
     if (!msg.guild) return; // bot will only reply if message is sent in the guild (server)
-    if (!msg.content.startsWith(PREFIX)) return; // bot will only reply if the message starts with the specified prefix
-    if (msg.channel.id != CHANNEL_ID) return; // bot will only reply in the specified channel
+    if (msg.channel.id != process.env.DRINKING_GAME_BOT_CHANNEL_ID) return; // bot will only reply in the specified channel
 
     // Handle arguments given
     switch (args[0].toLowerCase()) {
@@ -31,7 +26,7 @@ bot.on('message', (msg) => {
         case 'wyr':
             if (args[1]) {
                 if (!msg.content.includes("or")) return msg.channel.send("Error: The following format must be used: !wyr alternative1 or alternative2").then(message => message.delete({ timeout: 6000 }))
-                let userQuestion = msg.content.substring(PREFIX.length + args[0].length) // extract the question from the command 
+                let userQuestion = msg.content.substring(args[0].length) // extract the question from the command 
                 wyr.play(msg, userQuestion)
             }
 
@@ -43,7 +38,7 @@ bot.on('message', (msg) => {
 
             // use question from user
             if (args[1]) {
-                let userQuestion = msg.content.substring(PREFIX.length + args[0].length) // extract the question from the command 
+                let userQuestion = msg.content.substring(args[0].length) // extract the question from the command 
                 nhie.play(msg, userQuestion)
             }
 
